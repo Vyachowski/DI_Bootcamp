b91b8018e2b184145118ddf14ed10b43107e468e
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-05-07 13:50:39 +0300

    Add formatting for Gold exercises

--- week-2/day-1/ExercisesGold/exercises-gold.py
+++ week-2/day-1/ExercisesGold/exercises-gold.py
@@ -1,8 +1,15 @@
 # Exercise 1
+# Print the following output in one line of code:
 print('Hello world\n'*5)
 print('I love python\n'*5)
 
 # Exercise 2
+# 1) Ask the user to input a month (1 to 12).
+# 2) Display the season of the month received :
+#    Spring runs from March (3) to May (5)
+#    Summer runs from June (6) to August (8)
+#    Autumn runs from September (9) to November (11)
+#    Winter runs from December (12) to February (2)
 monthNumber = int(input("Please enter a number of a month (from 1 to 12): "))
 if (monthNumber == 12 or monthNumber < 3):
   print('It is winter now!')
