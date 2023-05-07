49000f590d98f3a4c4e5820b931c5b099d936cd6
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-05-07 10:33:28 +0300

    Complete necessary exercises

---
+++ week-2/day-1/exercises.py
@@ -0,0 +1,54 @@
+# Exercise 1
+print('Hello world')
+print('Hello world')
+print('Hello world')
+print('Hello world')
+print('Hello world\n')
+
+# Exercise 2
+print((99^3)*8)
+
+# Exercise 3
+print(5 < 3)
+print(3 == 3)
+print(3 == "3")
+# print('3' > 3)
+print("Hello" == "hello")
+
+# Exercise 4
+computer_brand = 'Apple'
+print(f'I have an {computer_brand} computer')
+
+# Exercise 5
+name = 'Slava'
+age = 34
+shoe_size = 42
+info = (f'Surprise is that {name} is {age} years old and it\'s so close to his {shoe_size} shoe size')
+print(info)
+
+# Exercise 6
+a = 3
+b = 6
+
+if (a < b):
+  print("Hello World!")
+
+# Exercise 7
+num = int(input("Please enter a number: "))
+if (num % 2 == 0):
+  print('It\'s even!')
+else: print('It\'s odd!')
+
+
+# Exercise 8
+my_name = 'Slava'
+name = str(input("Please enter your name, my friend: "))
+
+if (name == my_name):
+  print('Wow! You parents are the best with a great sense of beaty persons in the whole world!')
+
+# Exercise 9
+height = int(input("Please enter your height(in inches): "))
+if (height > 145):
+  print('You are tall enough to ride!')
+else: print('I am sorry, but this roller coaster it\'s only for your brother')
