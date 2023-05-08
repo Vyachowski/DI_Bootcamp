5ba59eea249ceb3d9f3b109d7bf360585801dd8f
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-05-08 20:49:36 +0300

    Add new exercises

--- week-2/day-2/ExercisesXP/exercises.py
+++ week-2/day-2/ExercisesXP/exercises.py
@@ -42,7 +42,31 @@ float_list = list((1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5))
 print(float_list)
 print(type(float_list[0]))
 
-# EXERCISE 4
-# Use a for loop to print all numbers from 1 to 20, inclusive.
-# Using a for loop, that loops from 1 to 20(inclusive), print out every element which has an even index.
+# EXERCISE 5
+# 1) Use a for loop to print all numbers from 1 to 20, inclusive.
+# 2) Using a for loop, that loops from 1 to 20(inclusive), print out every element which has an even index.
+for number in range(1, 21):
+  print(number)
+for number in range(1, 21):
+  print(number) if  number % 2 == 0 else False
 
+# EXERCISE 6
+# Write a while loop that will continuously ask the user for their name, unless the input is equal to your name.
+name = 'slava'
+answer = ''
+while (answer != name):
+  answer = input('May I have your name, sir: ').lower()
+
+# EXERCISE 7
+# Ask the user to input their favorite fruit(s) (one or several fruits).
+# Hint : Use the built in input method. Ask the user to separate the fruits with a single space, eg. 'apple mango cherry'.
+# Store the favorite fruit(s) in a list (convert the string of words into a list of words).
+# Now that we have a list of fruits, ask the user to input a name of any fruit.
+# If the user’s input is in the favorite fruits list, print “You chose one of your favorite fruits! Enjoy!”.
+# If the user’s input is NOT in the list, print, “You chose a new fruit. I hope you enjoy”.
+fruit_list = input('May I know your favorites fruits, sir (separate it with a space, please): ').lower().split(' ')
+favourite_fruit = input('Name the fruit, sir (only one)): ').lower()
+if (favourite_fruit in fruit_list):
+    print('You chose one of your favorite fruits! Enjoy!')
+else: 
+   print('You chose a new fruit. I hope you enjoy')
