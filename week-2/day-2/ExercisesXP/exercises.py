f972657b4d69461be8c445b35ad6863941fe7785
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-05-09 14:50:42 +0300

    Rename an exercise

--- week-2/day-2/ExercisesXP/exercises.py
+++ week-2/day-2/ExercisesXP/exercises.py
@@ -124,7 +124,7 @@ for sandwich in finished_sandwiches:
   print(f'Your {sandwich} is ready!')
 
 
-# EXERCISE 11 : SANDWICH ORDERS
+# EXERCISE 11 : SANDWICH ORDERS ADVANCED
 # Using the list sandwich_orders from the previous exercise, make sure the sandwich ‘pastrami’ appears in the list at least three times.
 # Add code near the beginning of your program to print a message saying the deli has run out of pastrami, and then use a while loop to remove all occurrences of ‘pastrami’ from sandwich_orders.
 # Make sure no pastrami sandwiches end up in finished_sandwiches.
