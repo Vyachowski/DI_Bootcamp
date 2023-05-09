9251c161de3273011b854ecba33c53595e7f1cb9
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-05-10 00:07:28 +0300

    Clean file structure

--- week-2/day-3/DailyChallenge/daily.py
+++ week-2/day-3/DailyChallenge/daily.py
@@ -1,45 +1,54 @@
-import random
 # CHALLENGE 1
-# Using the input function, ask the user for a string. The string must be 10 characters long.
-# 1) Ask the user for a number and a length.
-# 2) Create a program that prints a list of multiples of the number until the list length reaches length.
-#    Eg: 
-#    number: 7 - length 5 ➞ [7, 14, 21, 28, 35]
-#    number: 12 - length 10 ➞ [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
-#    number: 17 - length 6 ➞ [17, 34, 51, 68, 85, 102]
-start_point = int(input('For this trick I need a number. Your choice is: '))
-length = int(input('And a length: '))
-numbers = []
-acc = 0
-while length > len(numbers):
-  acc += start_point
-  numbers.append(acc)
-print(numbers)
+# 1) Ask a user for a word
+# 2) Write a program that creates a dictionary. This dictionary stores the indexes of each letter in a list.
+#    - Make sure the letters are the keys.
+#    - Make sure the letters are strings.
+#    - Make sure the indexes are stored in a list and those lists are values.
+# Examples:
+# "dodo" ➞ { "d": [0, 2], "o": [1, 3] }
+# "froggy" ➞ { "f":  [0], "r": [1], "o": [2], "g": [3, 4], "y": [5] }
+# "grapes" ➞ { "g": [0], "r": [1], "a": [2], "p": [3]}
+
 
 # CHALLENGE 2
-# Write a program that asks a string to the user, and display a new string with any duplicate consecutive letters removed.
+# Create a program that prints a list of the items you can afford in the store with the money you have in your wallet.
+# Sort the list in alphabetical order.
+# Return “Nothing” if you can’t afford anything from the store.
 # Examples
-# user's word : "ppoeemm" ➞ "poem"
-# user's word : "wiiiinnnnd" ➞ "wind"
-# user's word : "ttiiitllleeee" ➞ "title"
-# user's word : "cccccaaarrrbbonnnnn" ➞ "carbon"
-# Final strings won’t include words with double letters (e.g. “passing”, “lottery”).
 
-# First variant
-word = input('Give me a word, please, with any duplicate consecutive letters, for instance\'ppoeeemm\': ')
-letters_list = list(word)
-print(letters_list)
-final_list = []
-for letter in letters_list:
-  if (letter not in final_list):
-    final_list.append(letter)
-corrected_word1 = ''.join(final_list)
-print(corrected_word1)
+# The key is the product, the value is the price
 
-# Second variant
-word = input('Give me a word, please, with any duplicate consecutive letters, for instance\'ppoeeemm\': ')
-letters_list = list(word)
-removed_duplicates_list = list(dict.fromkeys(letters_list))
-print(removed_duplicates_list)
-corrected_word = ''.join(removed_duplicates_list)
-print(corrected_word)
+# items_purchase = {
+#   "Water": "$1",
+#   "Bread": "$3",
+#   "TV": "$1,000",
+#   "Fertilizer": "$20"
+# }
+
+# wallet = "$300"
+
+# ➞ ["Bread", "Fertilizer", "Water"]
+
+# items_purchase = {
+#   "Apple": "$4",
+#   "Honey": "$3",
+#   "Fan": "$14",
+#   "Bananas": "$4",
+#   "Pan": "$100",
+#   "Spoon": "$2"
+# }
+
+# wallet = "$100" 
+
+# ➞ ["Apple", "Bananas", "Fan", "Honey", "Pan", "Spoon"]
+
+# items_purchase = {
+#   "Phone": "$999",
+#   "Speakers": "$300",
+#   "Laptop": "$5,000",
+#   "PC": "$1200"
+# }
+
+# wallet = "$1" 
+
+# ➞ "Nothing"
