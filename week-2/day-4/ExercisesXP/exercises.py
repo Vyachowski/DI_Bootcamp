1b800312363d63b0e303a86203d88cf5f6f4b461
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-05-10 19:20:31 +0300

    Complete basic exercises

--- week-2/day-4/ExercisesXP/exercises.py
+++ week-2/day-4/ExercisesXP/exercises.py
@@ -87,27 +87,50 @@ show_magicians(magician_names)
 
 # 🌟 EXERCISE 7 : Temperature Advice
 ## Instructions
-## Create a function called get_random_temp().
-## This function should return an integer between -10 and 40 degrees (Celsius), selected at random.
-## Test your function to make sure it generates expected results.
-
-## Create a function called main().
-## Inside this function, call get_random_temp() to get a temperature, and store its value in a variable.
-## Inform the user of the temperature in a friendly message, eg. “The temperature right now is 32 degrees Celsius.”
-
-## Let’s add more functionality to the main() function. Write some friendly advice relating to the temperature:
-## below zero (eg. “Brrr, that’s freezing! Wear some extra layers today”)
-## between zero and 16 (eg. “Quite chilly! Don’t forget your coat”)
-## between 16 and 23
-## between 24 and 32
-## between 32 and 40
-
-## Change the get_random_temp() function:
-## Add a parameter to the function, named ‘season’.
-## Inside the function, instead of simply generating a random number between -10 and 40, set lower and upper limits based on the season, eg. if season is ‘winter’, temperatures should only fall between -10 and 16.
-## Now that we’ve changed get_random_temp(), let’s change the main() function:
-## Before calling get_random_temp(), we will need to decide on a season, so that we can call the function correctly. Ask the user to type in a season - ‘summer’, ‘autumn’ (you can use ‘fall’ if you prefer), ‘winter’, or ‘spring’.
-## Use the season as an argument when calling get_random_temp().
-
+## 1) Create a function called get_random_temp().
+## 2) This function should return an integer between -10 and 40 degrees (Celsius), selected at random.
+## 3) Test your function to make sure it generates expected results.
+## 4) Create a function called main().
+## 5) Inside this function, call get_random_temp() to get a temperature, and store its value in a variable.
+## 6) Inform the user of the temperature in a friendly message, eg. “The temperature right now is 32 degrees Celsius.”
+## 7) Let’s add more functionality to the main() function. Write some friendly advice relating to the temperature:
+##    – below zero (eg. “Brrr, that’s freezing! Wear some extra layers today”)
+##    – between zero and 16 (eg. “Quite chilly! Don’t forget your coat”)
+##    – between 16 and 23
+##    – between 24 and 32
+##    – between 32 and 40
+## 8) Change the get_random_temp() function:
+##    – Add a parameter to the function, named ‘season’.
+##    – Inside the function, instead of simply generating a random number between -10 and 40, set lower and upper limits based on the season, eg. if season is ‘winter’, temperatures should only fall between -10 and 16.
+## 11) Now that we’ve changed get_random_temp(), let’s change the main() function:
+##     – Before calling get_random_temp(), we will need to decide on a season, so that we can call the function correctly. Ask the user to type in a season - ‘summer’, ‘autumn’ (you can use ‘fall’ if you prefer), ‘winter’, or ‘spring’.
+##     – Use the season as an argument when calling get_random_temp().
 ## Bonus: Give the temperature as a floating-point number instead of an integer.
 ## Bonus: Instead of asking for the season, ask the user for the number of the month (1 = January, 12 = December). Determine the season according to the month.
+def get_random_temp(season):
+  if season == 'winter':
+    return random.randint(-10,16)
+  elif season == 'autumn':
+    return random.randint(17, 23)
+  elif season == 'spring':
+    return random.randint(24, 32)
+  elif season == 'summer':
+    return random.randint(33, 41)
+
+def main():
+  season = input('What season do you prefer? Pleasy type in: ')
+  temp = get_random_temp(season)
+  print(f'The temperature right now is {temp} degrees Celsius.')
+  if temp < 0:
+    print('Brrr, that’s freezing! Wear some extra layers today')
+  elif 0 < temp <= 16:
+    print('Quite chilly! Don’t forget your coat')
+  elif 16 < temp <= 23:
+    print('To coat or not to coat? Hard to decise')
+  elif 23 < temp <= 32:
+    print('Grab a hoodie for the evening, my friend.')
+  elif 32 < temp < 40:
+    print('Welcome to Eilat, please use a sun protection and wear a sunglasses!')
+
+print(get_random_temp('spring'))
+main()
