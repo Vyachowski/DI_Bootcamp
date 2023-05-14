22e150f8f9308c85fdbe85c91aeade0388310e89
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-05-14 08:32:32 +0300

    Add new fix for task from day-5

--- week_2/day_5/daily_challenge/daily.py
+++ week_2/day_5/daily_challenge/daily.py
@@ -3,7 +3,7 @@
 ## Instructions:
 ## 1) Write a program that accepts a comma separated sequence of words as input and prints the words in a comma-separated sequence 
 #     after sorting them alphabetically.
-## 2) Use List Comprehension
+## 2) Use List Comprehension (???)
 
 # DATA
 str = 'without,hello,bag,world'
@@ -14,5 +14,6 @@ str = 'without,hello,bag,world'
 def sorting(str, user_input = False):
     if user_input == True:
         str = input('Please type a comma-separated sequence of words: ')
-    return ','.join(sorted(str.split(',')))
+        sorted_list = ','.join(sorted(str.split(',')))
+    return sorted_list
 
