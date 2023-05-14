4a287b73290314bf9d1a01731564a3a5b1fa2b75
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-05-14 19:05:55 +0300

    Add third exercise todays

--- week_3/day_1/exercises/main.py
+++ week_3/day_1/exercises/main.py
@@ -27,35 +27,68 @@ oldest_cat = find_oldest_cat(cats)
 print(f'The oldest cat is {oldest_cat.name}, and he is {oldest_cat.age} years old.')
 
 # 🌟 Exercise 2 : Dogs
-# Instructions
-# Create a class called Dog.
-# In this class, create an __init__ method that takes two parameters : name and height. This function instantiates two attributes, which values are the parameters.
-# Create a method called bark that prints the following string “<dog_name> goes woof!”.
-# Create a method called jump that prints the following string “<dog_name> jumps <x> cm high!”. x is the height*2.
-# Outside of the class, create an object called davids_dog. His dog’s name is “Rex” and his height is 50cm.
-# Print the details of his dog (ie. name and height) and call the methods bark and jump.
-# Create an object called sarahs_dog. Her dog’s name is “Teacup” and his height is 20cm.
-# Print the details of her dog (ie. name and height) and call the methods bark and jump.
-# Create an if statement outside of the class to check which dog is bigger. Print the name of the bigger dog.
+# Instructions:
+# 1) Create a class called Dog.
+# 2) In this class, create an __init__ method that takes two parameters : name and height. 
+#    This function instantiates two attributes, which values are the parameters.
+# 3) Create a method called bark that prints the following string “<dog_name> goes woof!”.
+# 4) Create a method called jump that prints the following string “<dog_name> jumps <x> cm high!”. x is the height*2.
+# 5) Outside of the class, create an object called davids_dog. His dog’s name is “Rex” and his height is 50cm.
+# 6) Print the details of his dog (ie. name and height) and call the methods bark and jump.
+# 7) Create an object called sarahs_dog. Her dog’s name is “Teacup” and his height is 20cm.
+# 8) Print the details of her dog (ie. name and height) and call the methods bark and jump.
+# 9) Create an if statement outside of the class to check which dog is bigger. Print the name of the bigger dog.
+class Dog:
+  def __init__(self, name, height):
+    self.name = name
+    self.height = height
 
+  def bark(self):
+    print(f'{self.name} goes woof!')
+
+  def jump(self):
+    print(f'{self.name} jumps {(self.height) * 2} cm high!')
+
+davids_dog = Dog('Rex', 50)
+sarahs_dog = Dog('Teacup', 20)
+
+# OUTPUT
+
+print(davids_dog.name)
+print(davids_dog.height)
+davids_dog.bark()
+davids_dog.jump()
+
+print(sarahs_dog.name)
+print(sarahs_dog.height)
+sarahs_dog.bark()
+sarahs_dog.jump()
+
+biggest_dog = davids_dog if davids_dog.height > sarahs_dog.height else sarahs_dog
+
+print(biggest_dog.name)
 
 # 🌟 Exercise 3 : Who’s The Song Producer?
-# Instructions
-# Define a class called Song, it will show the lyrics of a song.
-# Its __init__() method should have two arguments: self and lyrics (a list).
-# Inside your class create a method called sing_me_a_song that prints each element of lyrics on its own line.
-# Create an object, for example:
+# Instructions:
+# 1) Define a class called Song, it will show the lyrics of a song.
+# 2) Its __init__() method should have two arguments: self and lyrics (a list).
+# 3) Inside your class create a method called sing_me_a_song that prints each element of lyrics on its own line.
+# 4) Create an object <stairway-to-heaven>
+# 5) Then, call the sing_me_a_song method. The output should be:
+#    – There’s a lady who's sure
+#    – all that glitters is gold
+#    – and she’s buying a stairway to heaven
+class Song:
+  def __init__(self, lyrics):
+    self.lyrics = lyrics
 
-# stairway= Song(["There’s a lady who's sure","all that glitters is gold", "and she’s buying a stairway to heaven"])
+  def sing_me_a_song(self):
+    for line in self.lyrics:
+      print(line)
 
+stairway = Song(["There’s a lady who's sure", "all that glitters is gold", "and she’s buying a stairway to heaven"])
 
-# Then, call the sing_me_a_song method. The output should be:
-
-# There’s a lady who's sure
-# all that glitters is gold
-# and she’s buying a stairway to heaven
-
-
+stairway.sing_me_a_song()
 # Exercise 4 : Afternoon At The Zoo
 # Instructions
 # Create a class called Zoo.
