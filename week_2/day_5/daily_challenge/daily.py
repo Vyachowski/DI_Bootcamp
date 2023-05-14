5fe7674e36f9418d4af84a487fbe2adeb328330f
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-05-14 09:48:29 +0300

    Add fix

--- week_2/day_5/daily_challenge/daily.py
+++ week_2/day_5/daily_challenge/daily.py
@@ -14,6 +14,5 @@ str = 'without,hello,bag,world'
 def sorting(str, user_input = False):
     if user_input == True:
         str = input('Please type a comma-separated sequence of words: ')
-        sorted_list = ','.join(sorted(str.split(',')))
+    sorted_list = ','.join(sorted(str.split(',')))
     return sorted_list
-
