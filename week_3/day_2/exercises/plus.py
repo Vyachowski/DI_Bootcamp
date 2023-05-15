76811bb5463d9f3c5827487e0c34bb83ba872c7d
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-05-15 22:54:37 +0300

    Add final exercise

--- week_3/day_2/exercises/plus.py
+++ week_3/day_2/exercises/plus.py
@@ -21,9 +21,9 @@ class Family:
     self.members = members
     self.name = last_name
 
-  def born(self, **birth_data):
+  def born(self, **data):
     newborn = {'name': '', 'age': 0, 'gender': '', 'is_child': True}
-    newborn.update(birth_data)
+    newborn.update(data)
     self.members.append(newborn)
     return 'Congratulions! Your family has a new member!'
 
@@ -57,15 +57,48 @@ kuzmini.family_presentation()
 # Instructions:
 # 1) Create a class called TheIncredibles. This class should inherit from the Family class:
 #    – This is no random family they are an incredible family, therefore we need to add the following keys to our dictionaries: power and incredible_name.
-#    – Initial members data:
+#    * Initial members data:
 #    [
 #      {'name':'Michael','age':35,'gender':'Male','is_child':False,'power': 'fly','incredible_name':'MikeFly'},
 #      {'name':'Sarah','age':32,'gender':'Female','is_child':False,'power': 'read minds','incredible_name':'SuperWoman'}
 #    ]
-# 2) Add a method called use_power, this method should print the power of a member only if they are over 18 years old. If not raise an exception (look up exceptions) which stated they are not over 18 years old.
+# 2) Add a method called use_power, this method should print the power of a member only if they are over 18 years old. 
+#    If not raise an exception (look up exceptions) which stated they are not over 18 years old.
 # 3) Add a method called incredible_presentation which :
 #   – Prints the family’s last name and all the members’ first name (ie. use the super() function, to call the family_presentation method)
 #   – Prints all the members’ incredible name and power.
 # 4) Call the incredible_presentation method.
 # 5) Use the born method inherited from the Family class to add Baby Jack with the following power: “Unknown Power”.
 # 6) Call the incredible_presentation method again.
+
+# CODE
+
+class TheIncredibles(Family):
+  def __init__(self, members, last_name):
+    super().__init__(members, last_name)
+
+  def use_power(self, name):
+    if self.is_18(name):
+      for member in self.members: 
+        if name in member.values(): print(member['power']) 
+    else: raise Exception('Sorry, no superpowers until 18 years old')
+
+  def incredible_presentation(self):
+    self.family_presentation()
+    print(f'It is {self.name} Incredible Family!\n\nFrom left to right:')
+    for member in self.members: print(f'{member["incredible_name"]} can {member["power"]}')
+
+# OUTPUT
+
+incredibles = [
+                {'name':'Michael','age':35,'gender':'Male','is_child':False,'power': 'fly','incredible_name':'MikeFly'},
+                {'name':'Sarah','age':32,'gender':'Female','is_child':False,'power': 'read minds','incredible_name':'SuperWoman'}
+            ]
+
+petrovi = TheIncredibles(incredibles, 'Petrovi')
+print(petrovi.members)
+petrovi.use_power('Sarah')
+petrovi.incredible_presentation()
+petrovi.born(name='Jack', gender='Male', power = 'Unknown_Power', incredible_name="Unknown")
+print(petrovi.members)
+petrovi.incredible_presentation()
