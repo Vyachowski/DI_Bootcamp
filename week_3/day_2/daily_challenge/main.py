6c9dc8bc7b336f4ee1433092ed66ff43a1205cab
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-05-16 22:09:25 +0300

    Some new fixes for Pagination

--- week_3/day_2/daily_challenge/main.py
+++ week_3/day_2/daily_challenge/main.py
@@ -10,25 +10,25 @@
 # 3) The Pagination class will have a few methods:
 #    – getVisibleItems() : returns a list of items visible depending on the pageSize
 #    – So for example we could use this method like this:
-#      * p.getVisibleItems() 
-#        ["a", "b", "c", "d"]
+#      p.getVisibleItems() 
+#      # ["a", "b", "c", "d"]
 # 4) You will have to implement various methods to go through the pages such as:
 #    – prevPage()
 #    – nextPage()
 #    – firstPage()
 #    – lastPage()
 #    – goToPage(pageNum)
-# Here’s a continuation of the example above using nextPage and lastPage:
-# alphabetList = list("abcdefghijklmnopqrstuvwxyz")
-# p = Pagination(alphabetList, 4)
-# p.getVisibleItems() 
-# # ["a", "b", "c", "d"]
-# p.nextPage()
-# p.getVisibleItems()
-# # ["e", "f", "g", "h"]
-# p.lastPage()
-# p.getVisibleItems()
-# # ["y", "z"]
+# 5) Here’s a continuation of the example above using nextPage and lastPage:
+#    – alphabetList = list("abcdefghijklmnopqrstuvwxyz")
+#    – p = Pagination(alphabetList, 4)
+#    – p.getVisibleItems() 
+#      # ["a", "b", "c", "d"]
+#      p.nextPage()
+#      p.getVisibleItems()
+#      # ["e", "f", "g", "h"]
+#      p.lastPage()
+#      p.getVisibleItems()
+#      # ["y", "z"]
 # Notes
 # The second argument (pageSize) could be a float, in that case just convert it to an int (this is also the case for the goToPage method)
 # The methods used to change page should be chainable, so you can call them one after the other like this: p.nextPage().nextPage()
@@ -70,20 +70,24 @@ class Pagination:
       self.__currentIndex = self.__lastIndex
 
     def goToPage(self, pageNum):
-      if 0 < pageNum <= self.totalPages:
+      if 0 < int(pageNum) <= self.totalPages:
         self.__currentIndex = pageNum - 1 
+      elif int(pageNum) > self.totalPages:
+        self.__currentIndex = self.__lastIndex
       else:
-        return 'Such page does not exist, sorry'
+        self.__currentIndex = 0
       
 # OUTPUT
 
 alphabetList = 'abcdefghijklmnopqrstuvwxyz'
 p = Pagination(alphabetList, 4)
 
+print('\nTEST #1:')
 print(p.getVisibleItems()) # -> ['a', 'b', 'c', 'd']
 print(p.totalPages)        # -> 7
 print(p.currentPage)       # -> 1
 
+print('\nTEST #2:')
 p.prevPage()               
 print(p.currentPage)       # -> 1
 print(p.getVisibleItems()) # -> ['a', 'b', 'c', 'd']
@@ -96,21 +100,28 @@ p.prevPage()
 print(p.currentPage)       # -> 1
 print(p.getVisibleItems()) # -> ['a', 'b', 'c', 'd']
 
+
+print('\nTEST #3:')
 p.nextPage()
 p.nextPage()
 p.nextPage()
 p.nextPage()
-p.nextPage()                 # -> Some extra .nextPage method to check "Out of Range"
+p.nextPage()                 
 print(p.currentPage)         # -> 6
 print(p.getVisibleItems())   # -> ['u', 'v', 'w', 'x']
 
 p.nextPage()
 p.nextPage()
-p.nextPage()
-print(p.currentPage)       # -> 7
-print(p.getVisibleItems()) # -> ['y', 'z']
+p.nextPage()                 # -> Some extra .nextPage method to check "Out of Range"
+print(p.currentPage)         # -> 7
+print(p.getVisibleItems())   # -> ['y', 'z']
 
-
-# p.nextPage()
-# print(p.currentPage) # -> 15
-# print(p.getVisibleItems()) # -> ['m', 'n', 'o', 'p']
+print('\nTEST #4:')
+p.goToPage(1)
+print(p.getVisibleItems())   # -> ['a', 'b', 'c', 'd']
+p.lastPage()
+print(p.getVisibleItems())   # -> ['y', 'z']
+p.goToPage(-100)
+print(p.getVisibleItems())   # -> ['a', 'b', 'c', 'd']
+p.goToPage(100)
+print(p.getVisibleItems())   # -> ['y', 'z']
