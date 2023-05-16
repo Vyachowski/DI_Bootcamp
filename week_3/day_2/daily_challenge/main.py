0d5d1de8f5b046aad4f511fc5cca3fadf1ce1d9e
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-05-16 08:54:23 +0300

    Add daily exercise

--- week_3/day_2/daily_challenge/main.py
+++ week_3/day_2/daily_challenge/main.py
@@ -1,43 +1,36 @@
-import random
-# CHALLENGE 1
-# Using the input function, ask the user for a string. The string must be 10 characters long.
-# 1) Ask the user for a number and a length.
-# 2) Create a program that prints a list of multiples of the number until the list length reaches length.
-#    Eg: 
-#    number: 7 - length 5 ➞ [7, 14, 21, 28, 35]
-#    number: 12 - length 10 ➞ [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
-#    number: 17 - length 6 ➞ [17, 34, 51, 68, 85, 102]
-start_point = int(input('For this trick I need a number. Your choice is: '))
-length = int(input('And a length: '))
-numbers = []
-acc = 0
-while length > len(numbers):
-  acc += start_point
-  numbers.append(acc)
-print(numbers)
-
-# CHALLENGE 2
-# Write a program that asks a string to the user, and display a new string with any duplicate consecutive letters removed.
-# Examples
-# user's word : "ppoeemm" ➞ "poem"
-# user's word : "wiiiinnnnd" ➞ "wind"
-# user's word : "ttiiitllleeee" ➞ "title"
-# user's word : "cccccaaarrrbbonnnnn" ➞ "carbon"
-# Final strings won’t include words with double letters (e.g. “passing”, “lottery”).
-
-# First variant
-word = input('Give me a word, please, with any duplicate consecutive letters, for instance\'ppoeeemm\': ')
-letters_list = list(word)
-final_list = []
-for letter in letters_list:
-  if (letter not in final_list):
-    final_list.append(letter)
-corrected_word1 = ''.join(final_list)
-print(corrected_word1)
-
-# Second variant
-word = input('Give me a word, please, with any duplicate consecutive letters, for instance\'ppoeeemm\': ')
-letters_list = list(word)
-removed_duplicates_list = list(dict.fromkeys(letters_list))
-corrected_word = ''.join(removed_duplicates_list)
-print(corrected_word)
+# --- CHALLENGE 1 --- #
+# Instructions :
+# 1) Create a class to handle paginated content in a website. A pagination is used to divide long lists of content in a series of pages.
+# 2) The Pagination class will accept 2 parameters:
+#    – items (default: None): It will contain a list of contents to paginate.
+#    – pageSize (default: 10): The amount of items to show in each page.
+#    So for example we could initialize our pagination like this:
+#    * alphabetList = list("abcdefghijklmnopqrstuvwxyz")
+#    * p = Pagination(alphabetList, 4)
+# 3) The Pagination class will have a few methods:
+#    – getVisibleItems() : returns a list of items visible depending on the pageSize
+#    – So for example we could use this method like this:
+#      * p.getVisibleItems() 
+#        ["a", "b", "c", "d"]
+# 4) You will have to implement various methods to go through the pages such as:
+#    – prevPage()
+#    – nextPage()
+#    – firstPage()
+#    – lastPage()
+#    – goToPage(pageNum)
+# Here’s a continuation of the example above using nextPage and lastPage:
+# alphabetList = list("abcdefghijklmnopqrstuvwxyz")
+# p = Pagination(alphabetList, 4)
+# p.getVisibleItems() 
+# # ["a", "b", "c", "d"]
+# p.nextPage()
+# p.getVisibleItems()
+# # ["e", "f", "g", "h"]
+# p.lastPage()
+# p.getVisibleItems()
+# # ["y", "z"]
+# Notes
+# The second argument (pageSize) could be a float, in that case just convert it to an int (this is also the case for the goToPage method)
+# The methods used to change page should be chainable, so you can call them one after the other like this: p.nextPage().nextPage()
+# Please set the p.totalPages and p.currentPage attributes to the appropriate number as there cannot be a page 0.
+# If a page is outside of the totalPages attribute, then the goToPage method should go to the closest page to the number provided (e.g. there are only 5 total pages, but p.goToPage(10) is given: the p.currentPage should be set to 5; if 0 or a negative number is given, p.currentPage should be set to 1).
