21a0b96ca48c26b164a4df37829822101012b554
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-05-16 22:34:52 +0300

    Formatting

--- week_3/day_2/daily_challenge/main.py
+++ week_3/day_2/daily_challenge/main.py
@@ -1,5 +1,5 @@
 # --- CHALLENGE 1 --- #
-# Instructions :
+# Instructions:
 # 1) Create a class to handle paginated content in a website. A pagination is used to divide long lists of content in a series of pages.
 # 2) The Pagination class will accept 2 parameters:
 #    – items (default: None): It will contain a list of contents to paginate.
@@ -29,13 +29,13 @@
 #      p.lastPage()
 #      p.getVisibleItems()
 #      # ["y", "z"]
-# Notes
-# The second argument (pageSize) could be a float, in that case just convert it to an int (this is also the case for the goToPage method)
-# The methods used to change page should be chainable, so you can call them one after the other like this: p.nextPage().nextPage()
-# Please set the p.totalPages and p.currentPage attributes to the appropriate number as there cannot be a page 0.
-# If a page is outside of the totalPages attribute, then the goToPage method should go to the closest page to the number provided 
-# (e.g. there are only 5 total pages, but p.goToPage(10) is given: the p.currentPage should be set to 5; if 0 or a negative number is given, 
-# p.currentPage should be set to 1).
+# Notes:
+# – The second argument (pageSize) could be a float, in that case just convert it to an int (this is also the case for the goToPage method)
+# – The methods used to change page should be chainable, so you can call them one after the other like this: p.nextPage().nextPage()
+# – Please set the p.totalPages and p.currentPage attributes to the appropriate number as there cannot be a page 0.
+# – If a page is outside of the totalPages attribute, then the goToPage method should go to the closest page to the number provided 
+#   (e.g. there are only 5 total pages, but p.goToPage(10) is given: the p.currentPage should be set to 5; if 0 or a negative number is given, 
+#    p.currentPage should be set to 1).
 
 # CODE
 
