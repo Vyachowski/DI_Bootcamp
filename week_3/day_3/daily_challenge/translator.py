6a995b0ba22dfaec71f9fdde14e7342fed76b024
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-05-17 15:41:49 +0300

    Translator exercise

--- week_3/day_3/daily_challenge/translator.py
+++ week_3/day_3/daily_challenge/translator.py
@@ -1,3 +1,25 @@
-# --- NO EXERCISE HERE YET --- #
+# --- Daily Challenge: Translator --- #
+# Time to finish: 
+# Instructions :
+# 1) Consider this list
+#    – french_words= ["Bonjour", "Au revoir", "Bienvenue", "A bientôt"] 
+# 2) Look at this result :
+#    – {"Bonjour": "Hello", "Au revoir": "Goodbye", "Bienvenue": "Welcome", "A bientôt": "See you soon"}
+# 3) You have to recreate the result using a translator module.
 
-# Obstacle: Stuck on the pagination exercise
+# IMPORT
+
+from googletrans import Translator
+
+# CODE
+
+translator = Translator()
+
+french_dict = {}
+french_words = ["Bonjour", "Au revoir", "Bienvenue", "A bientôt"] 
+translation = translator.translate("Bonjour", src='fr')
+
+for word in french_words:
+  french_dict[word] = translator.translate(word, src='fr').text
+
+print(french_dict) # -> {"Bonjour": "Hello", "Au revoir": "Goodbye", "Bienvenue": "Welcome", "A bientôt": "See you soon"}
