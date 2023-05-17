8b2a98b72ff2c74f4d6a2dc5dc532aa0f3ff78e8
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-05-17 22:21:08 +0300

    Last efforts

--- week_3/day_3/exercises/main.py
+++ week_3/day_3/exercises/main.py
@@ -1,5 +1,5 @@
 # --- 🌟 Exercise 1 : Built-In Functions --- #
-# Instructions
+# Instructions:
 # Python has many built-in functions.
 #  – If you feel that you don’t know how to properly implement them take a look at the python documentation online.
 # 1) Write a program which prints the results of the following python built-in functions: abs(), int(), input().
@@ -39,7 +39,7 @@ print(display_int_result.__doc__)     # -> Docs...
 print(display_input_result.__doc__)   # -> Docs...
 
 # --- 🌟 Exercise 2: Currencies --- #
-# Instructions
+# Instructions:
 # 1) class Currency:
 #     def __init__(self, currency, amount):
 #       self.currency = currency
@@ -47,41 +47,69 @@ print(display_input_result.__doc__)   # -> Docs...
 #     # Your code starts HERE
 # Using the code above, implement the relevant methods and dunder methods which will output the results below.
 # Hint : When adding 2 currencies which don’t share the same label you should raise an error.
-# >>> c1 = Currency('dollar', 5)
-# >>> c2 = Currency('dollar', 10)
-# >>> c3 = Currency('shekel', 1)
-# >>> c4 = Currency('shekel', 10)
+#:c1 = Currency('dollar', 5)
+#:c2 = Currency('dollar', 10)
+#:c3 = Currency('shekel', 1)
+#:c4 = Currency('shekel', 10)
 
-# >>> str(c1)
+#:str(c1)
 # '5 dollars'
 
-# >>> int(c1)
+#:int(c1)
 # 5
 
-# >>> repr(c1)
+#:repr(c1)
 # '5 dollars'
 
-# >>> c1 + 5
+#:c1 + 5
 # 10
 
-# >>> c1 + c2
+#:c1 + c2
 # 15
 
-# >>> c1 
+#:c1 
 # 5 dollars
 
-# >>> c1 += 5
-# >>> c1
+#:c1 += 5
+#:c1
 # 10 dollars
 
-# >>> c1 += c2
-# >>> c1
+#:c1 += c2
+#:c1
 # 20 dollars
 
-# >>> c1 + c3
+#:c1 + c3
 # TypeError: Cannot add between Currency type <dollar> and <shekel>
 
 class Currency:
   def __init__(self, currency, amount):
     self.currency = currency
     self.amount = amount
+
+
+# OUTPUT
+
+c1 = Currency('dollar', 5)
+c2 = Currency('dollar', 10)
+c3 = Currency('shekel', 1)
+c4 = Currency('shekel', 10)
+
+str(c1)   # -> '5 dollars'
+
+int(c1)   # -> 5
+
+repr(c1)  # -> '5 dollars'
+
+c1 + 5    # -> 10
+
+c1 + c2   # -> 15
+
+c1        # -> 5 dollars
+
+c1 += 5
+c1        # -> 10 dollars
+
+c1 += c2
+c1        # -> 20 dollars
+
+c1 + c3   # -> TypeError: Cannot add between Currency type <dollar> and <shekel>
