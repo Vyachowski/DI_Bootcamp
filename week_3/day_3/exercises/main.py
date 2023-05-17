baffa4da51f449ffdab559d02243b18e95dc07e8
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-05-17 21:41:07 +0300

    File moving

--- week_3/day_3/exercises/main.py
+++ week_3/day_3/exercises/main.py
@@ -1,15 +1,43 @@
-# -- NOT READY YET -- #
-# REASON: TOO MUCH TIME SPENT ON PAGINATION EXERCISE
-
-# --- Exercise 1 : Built-In Functions --- #
+# --- 🌟 Exercise 1 : Built-In Functions --- #
 # Instructions
 # Python has many built-in functions.
 #  – If you feel that you don’t know how to properly implement them take a look at the python documentation online.
 # 1) Write a program which prints the results of the following python built-in functions: abs(), int(), input().
 # 2) Using the __doc__ dunder method create your own documentation which explains the execution of your code. Take a look at the doc method on google for help.
 
+# CODE
 
-# 🌟 Exercise 2: Currencies
+def display_abs_result(num):
+  '''
+  This function displays the absolute value of the given number. 
+  If the number is positive or zero, it prints the number itself. 
+  If the number is negative, it prints the negation of the number.
+  '''
+  print (num if num >= 0 else -num)
+
+def display_int_result(value):
+  '''
+  This function prints the integer representation of the given value. 
+  It discards any decimal places and outputs the integer part of the value.
+  '''
+  print (int(value))
+
+def display_input_result(value):
+  '''
+  This function prompts the user for input by displaying the given value as a prompt.
+  It then waits for the user to enter a value and press Enter. 
+  Once the user input is received, it is printed to the console.
+  '''
+  print (input(value))
+
+# OUTPUT
+print(display_abs_result.__doc__)   # -> Docs...
+
+print(display_int_result.__doc__)   # -> Docs...
+
+print(display_input_result.__doc__)   # -> Docs...
+
+# --- 🌟 Exercise 2: Currencies --- #
 # Instructions
 # 1) class Currency:
 #     def __init__(self, currency, amount):
