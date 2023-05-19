45056b440058e5dc3ddbf48c029e6751f309b70d
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-05-19 14:22:45 +0300

    Some update for import exercises

--- week_3/day_3/exercises/plus/plus.py
+++ week_3/day_3/exercises/plus/plus.py
@@ -24,6 +24,10 @@ def guess_random_number(user_number):
 # Hint: use the string module
 import string
 
+letters = string.ascii_uppercase + string.ascii_lowercase
+random_string = ''.join([random.choice(letters) for _ in range(5)])
+
+print(random_string) # -> e.g. 'fELch'
 
 # --- 🌟 Exercise 4 : Current Date --- #
 # Instructions
