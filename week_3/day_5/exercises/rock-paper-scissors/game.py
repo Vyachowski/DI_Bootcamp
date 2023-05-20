c5788fa2571ce3eee7ec712a94c93728baf3e47b
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-05-20 12:38:24 +0300

    Add first part of the game R-P-S

--- week_3/day_5/exercises/rock-paper-scissors/game.py
+++ week_3/day_5/exercises/rock-paper-scissors/game.py
@@ -7,9 +7,9 @@ class Game:
 
   def __init__(self) -> None:
     self.__move_variants = { 
-      'rock': ['rock', 'rok', 'roc', 'r'],
-      'paper': ['paper', 'p'],
-      'scissors': ['scissors', 'scissor', 'scisors', 'scisors', 's']
+      'rock': ['rock', 'rok', 'roc', 'r', '1'],
+      'paper': ['paper', 'p', '2'],
+      'scissors': ['scissors', 'scissor', 'scisors', 'scisors', 's', '3']
       }
     self.__all_moves_list = sum(self.__move_variants.values(), [])
 
@@ -17,33 +17,36 @@ class Game:
     user_variant = ''
 
     while user_variant not in self.__all_moves_list:
-      user_variant = input('Selecet [r]ock, [p]aper or [s]cissors: ') # Need to add a code for printing errors
-    user_answer = ''.join([key for key, values in self.__move_variants.items() if user_variant in values])
-    return user_answer
+      user_variant = input('Selecet [r]ock, [p]aper or [s]cissors: ') # Maybe need to add a code for printing errors
+    user_item = ''.join([key for key, values in self.__move_variants.items() if user_variant in values])
+    return user_item
   
   def get_computer_item(self):
-    computer_answer = random.choice(list(self.__move_variants.keys()))
-    return computer_answer
+    computer_item = random.choice(list(self.__move_variants.keys()))
+    return computer_item
 
-game = Game()
-print(game.get_user_item())
-print(game.get_computer_item())
+  def get_game_result(self, user_item, computer_item):
+    if user_item == computer_item:
+      return 'draw'
+    elif (user_item == 'rock' and computer_item == 'scissors') or \
+         (user_item == 'paper' and computer_item == 'rock') or \
+         (user_item == 'scissors' and computer_item == 'paper'):
+      return 'win'
+    else:
+      return 'loss'
 
+  def play(self):
+    user_item = self.get_user_item()
+    computer_item = self.get_computer_item()
+    result = self.get_game_result(user_item, computer_item)
 
-# get_computer_item(self) – Select rock/paper/scissors at random for the computer. Return the item at the end of the function. Use python’s random.choice() function (read about it online).
+    print(f'You selected {user_item}. The computer selected {computer_item}.')
 
-# get_game_result(self, user_item, computer_item) – Determine the result of the game.
-# Parameters:
-# user_item – the user’s chosen item (rock/paper/scissors)
-# computer_item – the computer’s chosen (random) item (rock/paper/scissors)
-# Return either win, draw, or loss. Where win means that the user has won, draw means the user and the computer got the same item, and loss means that the user has lost.
+    if result == 'win':
+        print('You win!')
+    elif result == 'draw':
+        print('It\'s a draw!')
+    else:
+        print('You lose!')
 
-# play(self) – the function that will be called from outside the class (ie. from rock-paper-scissors.py). It will do 3 things:
-# Get the user’s item (rock/paper/scissors) and remember it
-
-# Get a random item for the computer (rock/paper/scissors) and remember it
-
-# Determine the results of the game by comparing the user’s item and the computer’s item
-# Print the output of the game; something like this: “You selected rock. The computer selected paper. You lose”, “You selected scissors. The computer selected scissors. You drew!”
-
-# Return the results of the game as a string: win;draw;loss;, where win means that the user has won, draw means the user and the computer got the same item, and loss means that the user has lost.
+    return result
