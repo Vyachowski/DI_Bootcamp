437fbfc2f459dba4c517772f79edd4986c1c60c4
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-05-23 08:33:13 +0300

    Add exercise

--- week_4/day_2/exercises/XP.sql
+++ week_4/day_2/exercises/XP.sql
@@ -39,3 +39,24 @@ SELECT * FROM ITEMS WHERE PRICE <= 80 ORDER by PRICE DESC
 SELECT FIRST_NAME, LAST_NAME  FROM CUSTOMERS ORDER BY FIRST_NAME ASC LIMIT 3
 
 SELECT LAST_NAME  FROM CUSTOMERS ORDER BY LAST_NAME DESC
+
+-- PART II / Five lines are missing, still learning a topic/
+
+SELECT * FROM customer;
+
+SELECT CONCAT(first_name, ' ', last_name) AS full_name FROM customer;
+
+SELECT DISTINCT create_date FROM customer;
+
+SELECT * FROM customer ORDER BY first_name DESC;
+
+SELECT film_id, title, description, release_year, rental_rate FROM film ORDER BY rental_rate;
+
+SELECT address, phone FROM address WHERE district='Texas';
+
+SELECT * FROM film WHERE film_id=15 OR film_id=150;
+
+SELECT film_id, title, description, length, rental_rate FROM film WHERE title='Kiss Glory';
+
+SELECT film_id, title, description, length, rental_rate FROM film WHERE title LIKE 'Ki%';
+
