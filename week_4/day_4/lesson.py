088c169a8bef1e6d44ee5fb689475793731e9734
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-05-24 15:44:08 +0300

    Add all tasks from day-2

--- week_4/day_4/lesson.py
+++ week_4/day_4/lesson.py
@@ -1,1 +1,27 @@
-# --- NO EXERCISE HERE YET --- #
+import psycopg2
+
+# Conncetion to a database
+
+connection = psycopg2.connect(
+    database="Hollywood",
+    user='postgres',
+    password='root',
+    host='localhost',
+    port='5432'
+)
+
+cursor = connection.cursor()
+
+query = 'SELECT * FROM actor'
+
+cursor.execute(query)
+
+result = cursor.fetchall()
+
+print(result)
+
+cursor.close()
+connection.close()
+
+def get_all_actors():
+  query = ''
