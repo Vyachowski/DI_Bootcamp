04230d53bc12f87fdd30fe68289aba5733de808e
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-05-24 15:59:49 +0300

    Answer for quiz

--- week_4/day_2/daily_challenge/daily.sql
+++ week_4/day_2/daily_challenge/daily.sql
@@ -1,1 +1,19 @@
--- NOT READY YET / Preparing exercises
+-- Result of the output:
+--    SELECT COUNT(*) 
+--    FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NULL )
+-- ANSWER: 0, because we can't have a null as a condition
+
+-- Result of the output:
+--    SELECT COUNT(*) 
+--    FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id = 5 )
+-- ANSWER: 2, all values except null and 5
+
+-- Result of the output:
+--    SELECT COUNT(*) 
+--    FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab )
+-- ANSWER: 2, same as previous
+
+-- Result of the output:
+--    SELECT COUNT(*) 
+--    FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id = 5 )
+-- ANSWER: 2, same as previous
