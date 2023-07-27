2fa89c42a14eca828e3a9246504b8bea46ac5ace
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-07-27 16:08:43 +0300

    add daily challenge

--- week_4/day_3/daily_challenge/daily.sql
+++ week_4/day_3/daily_challenge/daily.sql
@@ -31,8 +31,28 @@ CREATE TABLE CustomerProfile (
     customer_id INT REFERENCES Customer (id)
 );
 
+INSERT INTO Customer (first_name, last_name)
+VALUES ('John', 'Doe'),
+       ('Jerome', 'Lalu'),
+       ('Lea', 'Rive');
 
+INSERT INTO CustomerProfile (isLoggedIn, customer_id)
+VALUES (true, (SELECT id FROM Customer WHERE first_name = 'John')),
+       (false, (SELECT id FROM Customer WHERE first_name = 'Jerome'));
 
+SELECT c.first_name
+FROM Customer c
+JOIN CustomerProfile cp ON c.id = cp.customer_id
+WHERE cp.isLoggedIn = true;
+
+SELECT c.first_name, cp.isLoggedIn
+FROM Customer c
+LEFT JOIN CustomerProfile cp ON c.id = cp.customer_id;
+
+SELECT COUNT(*)
+FROM Customer c
+LEFT JOIN CustomerProfile cp ON c.id = cp.customer_id
+WHERE cp.isLoggedIn IS NULL;
 
 -- Part II:
 
@@ -69,3 +89,57 @@ CREATE TABLE CustomerProfile (
 -- Select the name of the student and the title of the borrowed books
 -- Select the average age of the children, that borrowed the book Alice in Wonderland
 -- Delete a student from the Student table, what happened in the junction table ?
+
+CREATE TABLE Book (
+    book_id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL
+);
+
+INSERT INTO Book (title, author)
+VALUES ('Alice In Wonderland', 'Lewis Carroll'),
+       ('Harry Potter', 'J.K Rowling'),
+       ('To kill a mockingbird', 'Harper Lee');
+
+CREATE TABLE Student (
+    student_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    age INTEGER CHECK (age <= 15)
+);
+
+INSERT INTO Student (name, age)
+VALUES ('John', 12),
+       ('Lera', 11),
+       ('Patrick', 10),
+       ('Bob', 14);
+
+CREATE TABLE Library (
+    book_fk_id INT REFERENCES Book (book_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    student_fk_id INT REFERENCES Student (student_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    borrowed_date DATE,
+    PRIMARY KEY (book_fk_id, student_fk_id)
+);
+
+INSERT INTO Library (book_fk_id, student_fk_id, borrowed_date)
+VALUES ((SELECT book_id FROM Book WHERE title = 'Alice In Wonderland'), (SELECT student_id FROM Student WHERE name = 'John'), '2022-02-15'),
+       ((SELECT book_id FROM Book WHERE title = 'To kill a mockingbird'), (SELECT student_id FROM Student WHERE name = 'Bob'), '2021-03-03'),
+       ((SELECT book_id FROM Book WHERE title = 'Alice In Wonderland'), (SELECT student_id FROM Student WHERE name = 'Lera'), '2021-05-23'),
+       ((SELECT book_id FROM Book WHERE title = 'Harry Potter'), (SELECT student_id FROM Student WHERE name = 'Bob'), '2021-08-12');
+
+SELECT *
+FROM Library;
+
+SELECT s.name AS student_name, b.title AS book_title
+FROM Library l
+JOIN Student s ON l.student_fk_id = s.student_id
+JOIN Book b ON l.book_fk_id = b.book_id;
+
+SELECT AVG(age) AS average_age
+FROM Library
+         JOIN Student ON Library.student_fk_id = Student.student_id
+         JOIN Book ON Library.book_fk_id = Book.book_id
+WHERE Book.title = 'Alice In Wonderland';
+
+DELETE from Student
+WHERE name='Bob';
+--    Records related to Bob will be deleted in Library.
