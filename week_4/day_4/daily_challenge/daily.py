d8fda4d32bb0f1b2a8c8a08391c33792c42e2b0c
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-08-03 11:52:55 +0300

    Delete excess comment

--- week_4/day_4/daily_challenge/daily.py
+++ week_4/day_4/daily_challenge/daily.py
@@ -1,8 +1,6 @@
-# NOT READY YET, YESTERDAY HAD AN ADDITIONAL CLASS IN COLLEGE SO I AM FINISHING IT AT THE MORNING
-
 # Instructions
-# Using this REST Countries API, create the functionality which will write 10 random countries to your database.
-# These are the attributes which you should populate your tables with: name, capital, flag, subregion, population.
+# 1) Using this REST Countries API, create the functionality which will write 10 random countries to your database.
+# 2) These are the attributes which you should populate your tables with: name, capital, flag, subregion, population.
     
 import requests, random, sqlite3, os
 
