88ef541f3f9aa3041ff8667dcc8e2a5f8f895e2e
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-08-13 21:34:08 +0300

    par of exercise

--- week_6/day_1/exercises/books-info/books_project/info/models.py
+++ week_6/day_1/exercises/books-info/books_project/info/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator, MaxValueValidator, MinLengthValidator
 from django.db import models
 
 # Create your models here.
@@ -6,18 +7,19 @@ from django.contrib.auth.models import User
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    published_date = models.DateField()
-    description = models.TextField()
-    page_count = models.IntegerField()
-    categories = models.CharField(max_length=100)
+    title = models.CharField(max_length=200, blank=False)
+    author = models.CharField(max_length=100, blank=False)
+    published_date = models.DateField(blank=False)
+    description = models.TextField(blank=False)
+    page_count = models.IntegerField(blank=False)
+    categories = models.CharField(max_length=100, blank=False)
     thumbnail_url = models.URLField()
+    objects = models.Manager()
 
 
 class BookReview(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    rating = models.IntegerField()
-    review_text = models.TextField()
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    review_text = models.TextField(validators=[MinLengthValidator(10)], blank=False)
 
--- week_6/day_1/exercises/books-info/books_project/info/views.py
+++ week_6/day_1/exercises/books-info/books_project/info/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from models import Book
+from django.http import JsonResponse
 # Create your views here.
+
+
+def list_books(request):
+    books = Book.objects.all()
+    data = [{'id': book.id, 'title': book.title, 'author': book.author} for book in books]
+    return JsonResponse(data)
+
+
+
