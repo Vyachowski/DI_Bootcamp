f2607a6ff6be1905b80ae2c04c7de973a28e3cc4
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-08-15 16:32:07 +0300

    model part

--- week_6/day_3/exercise/management_project/manager/models.py
+++ week_6/day_3/exercise/management_project/manager/models.py
@@ -17,7 +17,7 @@ class Employee(models.Model):
     department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f'{self.first_name} {self.last_name}'
 
 
 class Project(models.Model):
