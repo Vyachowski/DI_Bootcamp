cf51d50891b81f9b2670e80f8587beb463073bff
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-08-17 11:15:43 +0300

    daily_last

--- week_6/day_4/exercise/gold/script.js
+++ week_6/day_4/exercise/gold/script.js
@@ -19,11 +19,11 @@ let guestList = {
 
 // Given the object above where the key is the student’s name and the value is the country they are from.
 // Prompt the student for their name.
-const answer = prompt('What is your name stranger?').toLowerCase();
-if (answer in guestList) {
-  console.log(`Hi! I'm ${answer}, and I'm from ${guestList[answer]}.`)
-}
-console.log("Hi! I'm a guest.")
+// const answer = prompt('What is your name stranger?').toLowerCase();
+// if (answer in guestList) {
+//   console.log(`Hi! I'm ${answer}, and I'm from ${guestList[answer]}.`)
+// }
+// console.log("Hi! I'm a guest.")
 // If the name is in the object, console.log the name of the student and the country they come from.
 // For example: "Hi! I'm [name], and I'm from [country]."
 // Hint: You don’t need to use a for loop, just look up the statement if ... in
@@ -33,9 +33,20 @@ console.log("Hi! I'm a guest.")
 
 // Exercise 3 : Playing With Numbers
 // Instructions
-// let age = [20,5,12,43,98,55];
+let age = [20,5,12,43,98,55];
 // Requirements : Don’t use built-in Javascript methods to answer the following questions. You have to create the logic by yourself. Use simple for loops.
 
 
 // 1. Console.log the sum of all the numbers in the age array.
+let result = 0;
+for (const number of age) {
+  result += number;
+}
+console.log(result);
 // 2. Console.log the highest age in the array.
+result = 0;
+
+for (const number of age) {
+  number > result ? result = number : result;
+}
+console.log(result);
