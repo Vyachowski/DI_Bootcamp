8683d3ffef7a3a961d13821753a7dd536a3efc9a
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-08-17 14:47:20 +0300

    serializers

--- week_6/day_5/exam/django/academy/academy/school/serializers.py
+++ week_6/day_5/exam/django/academy/academy/school/serializers.py
@@ -1,2 +1,12 @@
 from rest_framework import serializers
 from .models import Course, Teacher
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+class TeacherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Teacher
+        fields = '__all__'
