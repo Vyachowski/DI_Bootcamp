28199f02f51d5e3036818950069029c591b9ccfc
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-08-20 10:13:18 +0300

    exercise 1

---
+++ week_7/day_1/lessons/script.js
@@ -0,0 +1,8 @@
+// Exercise 1
+function myAges(myAge) {
+  console.log('My mom\'s age', myAge * 2);
+  console.log('My dad\'s age', myAge * 2 * 1.2);
+}
+myAges(35); // -> My mom's age 70, My dad's age 84;
+
+// Exercise 1
