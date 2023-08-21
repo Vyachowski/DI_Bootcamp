82a7bc1f28b69e01c9894b2a3f8144ae2e7b1404
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-08-21 12:44:37 +0300

    exercise

--- week_7/day_2/lesson/index.js
+++ week_7/day_2/lesson/index.js
@@ -1,19 +1,33 @@
+let rowNumber = 3;
+
 function insertRow() {
   // Create column
   const newColumn = document.createElement('td');
   const newSecondColumn = document.createElement('td');
 
-  newColumn.textContent = 'Row cell';
-  newSecondColumn.textContent = 'Row cell';
+  newColumn.textContent = `Row${rowNumber} cell1`;
+  newSecondColumn.textContent = `Row${rowNumber} cell2`;
 
   // Create row
   const newRow = document.createElement('tr');
 
   // Add two columns to row
   const tableElement = document.querySelector('#sampleTable');
-  newRow.appendChild(newColumn);
-  newRow.appendChild(newSecondColumn);
+  newRow.append(newColumn, newSecondColumn);
   
   // Add rows to table
-  tableElement.appendChild(newRow);
+  tableElement.append(newRow);
+
+  // Row Number increment
+  rowNumber += 1;
+}
+
+// Exercise 2 – Change the button
+const button = document.querySelector('#jsstyle');
+
+function invisibleButton() {
+  button.textContent = 'Stop please';
+  button.style.color = 'red';
+  button.style.border = 0;
+  button.style.backgroundColor = 'transparent';
 }
