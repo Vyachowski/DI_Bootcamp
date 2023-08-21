1e907609980505b3f21938e9d78d369037f77cae
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-08-21 20:12:58 +0300

    sphere exercise

---
+++ week_7/day_2/exercises/sphere (exercise 4)/sphere.js
@@ -0,0 +1,10 @@
+document.getElementById("MyForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  const radius = parseFloat(document.getElementById("radius").value);
+  if (!isNaN(radius)) {
+      const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);;
+      document.getElementById("volume").value = volume.toFixed(2);
+  } else {
+      alert("Please enter a valid number");
+  }
+});
