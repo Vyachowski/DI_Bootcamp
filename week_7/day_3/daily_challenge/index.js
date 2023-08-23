73e0c5bceb9697a52f63996778ebdeb938b0c7c9
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-08-23 09:41:31 +0300

    daile changes

--- week_7/day_3/daily_challenge/index.js
+++ week_7/day_3/daily_challenge/index.js
@@ -1,26 +1,30 @@
 let client = 'John';
-// client = 'Betty';
 
 const groceries = {
     fruits : ["pear", "apple", "banana"],
     vegetables: ["tomatoes", "cucumber", "salad"],
     totalPrice : "20$",
-    // totalPrice : "35$",
+    totalPrice : "35$",
     other : {
         paid : true,
+        paid : false,
         meansOfPayment : ["cash", "creditCard"]
     }
 }
 
 // Create an arrow function named displayGroceries, that console.logs the 3 fruits from the groceries object. Use the forEach method.
 const displayGroceries = () => groceries.fruits.forEach(element => {console.log(element)});
-displayGroceries();
+// displayGroceries();
 
 // Create another arrow function named cloneGroceries.
 const cloneGroceries = () => {
-  const user = client;
+  let user = client;
+  user = 'Betty';
   const shopping = groceries;
+  // console.log(user);
+  console.log(shopping);
 };
+
 // In the function, create a variable named user that is a copy of the client variable. (Tip : make the user variable equal to the client variable)
 // Change the client variable to “Betty”. Will we also see this modification in the user variable ? Why ?
 
@@ -33,4 +37,10 @@ const cloneGroceries = () => {
 
 // Change the value of the paid key to false. Will we also see this modification in the shopping object ? Why ?
 
+// ANSWER: Yes, it will be changed, the same reason.
+
 // Invoke the cloneGroceries function.
+cloneGroceries();
+
+console.log('Old value: ', client);
+console.log('Old value: ', groceries);
