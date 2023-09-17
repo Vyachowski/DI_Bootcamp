96142bf86fe4fd3c73f1c33d35020ad080da57ab
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-09-17 21:13:44 +0300

    Daily exercise

--- week_10/day_3/daily_challenge/voting-app/src/App.js
+++ week_10/day_3/daily_challenge/voting-app/src/App.js
@@ -9,18 +9,10 @@ function App() {
     {name: "Java", votes: 0}
   ])
 
-  function increaseVote(languageName) {
-    setLanguages((previousVariant) => {
-      const updatedLanguages = [...previousVariant];
-      const language = updatedLanguages.find(
-        (lang) => lang.name === languageName
-      );
-      if (language) {
-        language.votes += 1;
-        console.log(updatedLanguages);
-      }
-      return updatedLanguages;
-    });
+  function increaseVote(number) {
+    const updatedLanguages = [...languages];
+    updatedLanguages[number].votes += 1;
+    setLanguages(updatedLanguages);
   }
 
   return (
@@ -29,22 +21,22 @@ function App() {
         <li className='language-item'>
           <span className='votes'>{languages[0].votes}</span>
           <span className='language-name'>Php</span>
-          <button className='votes-total' type='button' onClick={() => increaseVote('Php')}>Click here</button>
+          <button className='votes-total' type='button' onClick={() => increaseVote(0)}>Click here</button>
         </li>
         <li className='language-item'>
           <span className='votes'>{languages[1].votes}</span>
           <span className='language-name'>Python</span>
-          <button className='votes-total' type='button' onClick={() => increaseVote('Python')}>Click here</button>
+          <button className='votes-total' type='button' onClick={() => increaseVote(1)}>Click here</button>
         </li>
         <li className='language-item'>
           <span className='votes'>{languages[2].votes}</span>
           <span className='language-name'>JavaScript</span>
-          <button className='votes-total' type='button' onClick={() => increaseVote('JavaScript')}>Click here</button>
+          <button className='votes-total' type='button' onClick={() => increaseVote(2)}>Click here</button>
         </li>
         <li className='language-item'>
           <span className='votes'>{languages[3].votes}</span>
           <span className='language-name'>Java</span>
-          <button className='votes-total' type='button' onClick={() => increaseVote('Java')}>Click here</button>
+          <button className='votes-total' type='button' onClick={() => increaseVote(3)}>Click here</button>
         </li>
       </ul>
     </div>
